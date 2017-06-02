037fdeecd80ed874ca8b1251f02ca0a02186a581
ShengHua Wu <fantasy0404@gmail.com>
2017-06-02 09:23:58 +0800

    Define MessagingCell.

---
+++ Echo/Messaging/MessagingCell.swift
@@ -0,0 +1,15 @@
+//
+//  MessagingCell.swift
+//  Echo
+//
+//  Created by ShengHua Wu on 02/06/2017.
+//  Copyright © 2017 ShengHua Wu. All rights reserved.
+//
+
+import UIKit
+
+final class MessagingCell: UITableViewCell {
+    // MARK: Properties
+    @IBOutlet weak var bubbleImageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+}
