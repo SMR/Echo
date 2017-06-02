7d58edf746205218548499ffcbc548210dcae5ce
ShengHua Wu <fantasy0404@gmail.com>
2017-06-02 08:48:45 +0800

    Initialize project.

---
+++ Echo/Messaging/MessagingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MessagingViewController.swift
+//  Echo
+//
+//  Created by ShengHua Wu on 02/06/2017.
+//  Copyright © 2017 ShengHua Wu. All rights reserved.
+//
+
+import UIKit
+
+final class MessagingViewController: UIViewController {
+    // MARK: View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .brown
+    }
+}
